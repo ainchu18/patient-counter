0ada40eee56217fc512d3d6d578e69ef9d8fe6d4
ainchu18 <christianfordmacamchu18@gmail.com>
2021-08-12 17:21:17 +0000

    set up structure for tally page functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,100 @@ function totalAdultCount(){
     
 }
 
+// functions for tally page
+
+function addMinorTotalJanuary(){
+
+}
+
+function addAdultTotalJanuary(){
+
+}
+
+function addMinorTotalFebruary(){
+
+}
+
+function addAdultTotalFebruary(){
+
+}
+
+function addMinorTotalMarch(){
+
+}
+
+function addAdultTotalMarch(){
+
+}
+
+function addMinorTotalApril(){
+
+}
+
+function addAdultTotalApril(){
+
+}
+
+function addMinorTotalMay(){
+
+}
+
+function addAdultTotalMay(){
+
+}
+
+function addMinorTotalJune(){
+
+}
+
+function addAdultTotalJune(){
+
+}
+
+function addMinorTotalJuly(){
+
+}
+
+function addAdultTotalJuly(){
+
+}
+
+function addMinorTotalAugust(){
+
+}
+
+function addAdultTotalAugust(){
+
+}
+
+function addMinorTotalSeptember(){
+
+}
+
+function addAdultTotalSeptember(){
+
+}
+
+function addMinorTotalOctober(){
+
+}
+
+function addAdultTotalOctober(){
+
+}
+
+function addMinorTotalNovember(){
+
+}
+
+function addAdultTotalNovember(){
+
+}
+
+function addMinorTotalDecember(){
+
+}
+
+function addAdultTotalDecember(){
+
+}
