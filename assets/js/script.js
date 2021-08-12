0ba392b914eba08468092f3d557c7e33a34a25df
ainchu18 <christianfordmacamchu18@gmail.com>
2021-08-12 17:16:26 +0000

    set up structure for patient counter functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// functions  for patient counter/main page
+
+function incrementCount(){
+
+}
+
+function decrementCount(){
+    
+}
+
+function resetCount(){
+    
+}
+
+function totalMinorCount(){
+
+}
+
+function totalAdultCount(){
+    
+}
+
